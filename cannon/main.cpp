fcda15fefcfbd57e653ab35cb682825664e6019b
Zac <zactham@gmail.com>
2018-01-02 19:39:04 -0500

    Ball moves around the screen with the arrow keys

--- cannon/main.cpp
+++ cannon/main.cpp
@@ -5,26 +5,87 @@
 // we use the graphics API from SFML library
 #include <SFML/Graphics.hpp>
 
+
+//
+// our own ball class
+//
+class Ball
+{
+private:
+    sf::CircleShape ballShape;
+    int ballX;
+    int ballY;
+    int radius;
+    
+public:
+    // constructor
+    Ball()
+    {
+        ballX = 0;
+        ballY = 0;
+        radius = 0;
+    }
+    
+    void setX(int i)
+    {
+        ballX = i;
+    }
+    void setY(int i)
+    {
+        ballY = i;
+    }
+    int getX()
+    {
+        return ballX;
+    }
+    int getY()
+    {
+        return ballY;
+    }
+    void setSize(int r)
+    {
+        radius = r;
+    }
+    int getSize()
+    {
+        return radius;
+    }
+    
+    sf::CircleShape& getBall()
+    {
+        return ballShape;
+    }
+    
+    
+    
+};
+
 //
 // main function
 //
+
 int main()
 {
-	// set up ball shape
-	sf::CircleShape ballShape;
-	ballShape.setPosition(100, 100);
-	ballShape.setRadius(10.0f);
-	ballShape.setFillColor(sf::Color::Blue);
-
-	// set up window
-	sf::RenderWindow window{ { 800, 600}, "Cannon" };
-	window.setFramerateLimit(60);
-
-	//
-	// main game loop
-	//
-	while (window.isOpen())
-	{
+    int speed = 10;
+    
+    // set up ball shape
+    Ball ballShape;
+    ballShape.setX(500);
+    ballShape.setY(100);
+    ballShape.setSize(20);
+    ballShape.getBall().setPosition(ballShape.getX(), ballShape.getY());
+    ballShape.getBall().setRadius(ballShape.getSize());
+    ballShape.getBall().setFillColor(sf::Color::White);
+    
+    // set up window
+    sf::RenderWindow window{ { 800, 600}, "Cannon" };
+    window.setFramerateLimit(60);
+    
+    //
+    // main game loop
+    //
+    while (window.isOpen())
+    {
         // check for exit
         sf::Event event;
         while (window.pollEvent(event))
@@ -36,16 +97,47 @@ int main()
                 window.close();
             }
         }
-
+        
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            ballShape.setX(ballShape.getX());
+            ballShape.setY(ballShape.getY()-speed);
+            ballShape.getBall().setPosition(ballShape.getX(), ballShape.getY());
+            
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            
+            ballShape.setX(ballShape.getX());
+            ballShape.setY(ballShape.getY()+speed);
+            ballShape.getBall().setPosition(ballShape.getX(), ballShape.getY());
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            
+            ballShape.setX(ballShape.getX()-speed);
+            ballShape.setY(ballShape.getY());
+            ballShape.getBall().setPosition(ballShape.getX(), ballShape.getY());
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            
+            ballShape.setX(ballShape.getX()+speed);
+            ballShape.setY(ballShape.getY());
+            ballShape.getBall().setPosition(ballShape.getX(), ballShape.getY());
+        }
+        
+        
         // erase window
         window.clear();
         
         // update/draw objects
-		window.draw(ballShape);
-
-		// draw window
-		window.display();
-	}
-
+        window.draw(ballShape.getBall());
+        
+        // draw window
+        window.display();
+    }
+    
     return 0;
 }
+
